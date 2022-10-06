1f681a2abf2908551773661be12efa76ee984f94
Manuel <90761259+ManuelParma@users.noreply.github.com>
2022-10-06 11:49:10 +0200

    Punto 7

--- esercizi/02.2/parma-manuel/src/app/macchina/macchina.component.ts
+++ esercizi/02.2/parma-manuel/src/app/macchina/macchina.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class MacchinaComponent implements OnInit {
 
   @Input() macchina: string = '';
+  componenti: string[] = ['Bulloni', 'Viti', "Ferro"];
 
   constructor() { }
 
